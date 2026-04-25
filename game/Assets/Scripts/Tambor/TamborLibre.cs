d1c1ca354f837fcb1bbb25337385b5fd93d3b7aa
Timoteo <tabtim1015@gmail.com>
2026-04-25 17:59:25 -0600

    Feat: Tambor Libre

---
+++ game/Assets/Scripts/Tambor/TamborLibre.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TamborLibre : MonoBehaviour
+{
+    private AudioSource miAltavoz;
+
+    void Start()
+    {
+        // Buscamos el componente Audio Source al iniciar
+        miAltavoz = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if (Pointer.current != null && Pointer.current.press.wasPressedThisFrame)
+        {
+            Debug.Log("Clic detectado");
+            // Reproducir el sonido de tambor
+            ReproducirSonido();
+        }
+    }
+
+    void ReproducirSonido()
+    {
+        if (miAltavoz != null)
+        {
+            // Cambia el tono un poco hacia arriba o hacia abajo en cada clic
+            miAltavoz.pitch = Random.Range(0.9f, 1.1f);
+
+            // PlayOneShot es genial porque permite que el sonido 
+            // se solape si haces clics muy rápidos
+            miAltavoz.PlayOneShot(miAltavoz.clip);
+        }
+    }
+}
